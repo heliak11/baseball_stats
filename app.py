18a6ec12826cb188f0f896ba57a3181882b52d0f
Julien Gendron <julien_gendron@yahoo.ca>
2026-07-16 16:47:49 -0400

    api update

--- app.py
+++ app.py
@@ -690,8 +690,7 @@ elif choix_menu == "📸 Analyse IA":
                     try:
                         # Configuration de l'API avec le secret
                         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-                        model = genai.GenerativeModel('gemini-1.5-flash')
-                        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+                        model = genai.GenerativeModel('gemini-1.5-pro')
                         
                         prompt_sys = """
                         Tu es un expert en baseball (Baseball Québec).
